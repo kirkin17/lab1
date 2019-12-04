e300f6f96a2b68e0f1201ec46fa00d5734635497
kirkin17 <kirkin17@users.noreply.github.com>
2019-12-05 00:10:19 +0300

    v1

---
+++ lab1/lab1/lab1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	int x, y;
+	cin >> x >> y;
+	while (1)
+	{
+		if (x == 0 && y == 0) break;
+		if ((x <= 5) && (y >= 2) && ((y <= x)))
+		{
+			cout << "Included\n";
+		}
+		else { cout << "Not included\n"; };
+		cin >> x >> y;
+	}
+}
