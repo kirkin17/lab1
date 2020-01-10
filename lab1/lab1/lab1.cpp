cef12e540b17a8fda88376ce650885b0e86aa1ac
kirkin <Kirill Prokhorenko>
2020-01-11 02:31:43 +0300

    v2

--- lab1/lab1/lab1.cpp
+++ lab1/lab1/lab1.cpp
@@ -3,16 +3,20 @@
 using namespace std;
 int main()
 {
+	setlocale(LC_ALL,"russian");
 	int x, y;
-	cin >> x >> y;
+	cout << ("Введите х: ");
+	cin >> x
+	cout << ("Введите у: ");
+	cin >> y;
 	while (1)
 	{
 		if (x == 0 && y == 0) break;
 		if ((x <= 5) && (y >= 2) && ((y <= x)))
 		{
-			cout << "Included\n";
+			cout << "Точка входит в треугольник!\n";
 		}
-		else { cout << "Not included\n"; };
+		else { cout << "Точка НЕ входит в треугольник!\n"; };
 		cin >> x >> y;
 	}
 }
